8a439733af8511a492f878169f39ba9b1fdd3c1c
Ryosuke Iwanaga <ryosuke.iwanaga@autify.com>
2022-08-06 22:48:43 -0700

    Implement credentials

--- src/main/java/io/jenkins/plugins/autify/AutifyWebBuilder.java
+++ src/main/java/io/jenkins/plugins/autify/AutifyWebBuilder.java
@@ -5,28 +5,51 @@ import hudson.EnvVars;
 import hudson.Extension;
 import hudson.FilePath;
 import hudson.util.FormValidation;
+import hudson.util.ListBoxModel;
+import hudson.util.Secret;
 import hudson.model.AbstractProject;
+import hudson.model.Item;
+import hudson.model.Result;
 import hudson.model.Run;
 import hudson.model.TaskListener;
+import hudson.security.ACL;
 import hudson.tasks.Builder;
 import hudson.tasks.BuildStepDescriptor;
+
+import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.QueryParameter;
 
+import com.cloudbees.plugins.credentials.CredentialsMatchers;
+import com.cloudbees.plugins.credentials.CredentialsProvider;
+import com.cloudbees.plugins.credentials.common.StandardListBoxModel;
+
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.util.Collections;
+
+import jenkins.model.Jenkins;
 import jenkins.tasks.SimpleBuildStep;
+
+import org.apache.commons.lang.StringUtils;
 import org.jenkinsci.Symbol;
+import org.jenkinsci.plugins.plaincredentials.StringCredentials;
 import org.kohsuke.stapler.DataBoundSetter;
 
 public class AutifyWebBuilder extends Builder implements SimpleBuildStep {
 
+    private final String credentialsId;
     private final String autifyUrl;
     private boolean wait;
 
     @DataBoundConstructor
-    public AutifyWebBuilder(String autifyUrl) {
+    public AutifyWebBuilder(String credentialsId, String autifyUrl) {
+        this.credentialsId = credentialsId;
         this.autifyUrl = autifyUrl;
+    }
+
+    public String getCredentialsId() {
+        return credentialsId;
     }
 
     public String getAutifyUrl() {
@@ -44,6 +67,19 @@ public class AutifyWebBuilder extends Builder implements SimpleBuildStep {
 
     @Override
     public void perform(Run<?, ?> run, FilePath workspace, EnvVars env, Launcher launcher, TaskListener listener) throws InterruptedException, IOException {
+        StringCredentials credentials = CredentialsProvider.findCredentialById(
+            credentialsId,
+            StringCredentials.class,
+            run,
+            Collections.emptyList()
+        );
+        if (credentials == null) {
+            listener.getLogger().println("Cannot find any credentials for "+ credentialsId);
+            run.setResult(Result.FAILURE);
+            return;
+        }
+        String secretText = Secret.toString(credentials.getSecret());
+        listener.getLogger().println(secretText);
         if (wait) {
             listener.getLogger().println(autifyUrl + "wait");
         } else {
@@ -55,7 +91,52 @@ public class AutifyWebBuilder extends Builder implements SimpleBuildStep {
     @Extension
     public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
 
+        public ListBoxModel doFillCredentialsIdItems(@AncestorInPath Item item, @QueryParameter String credentialsId) {
+            StandardListBoxModel result = new StandardListBoxModel();
+            if (item == null) {
+                if (!Jenkins.get().hasPermission(Jenkins.ADMINISTER)) {
+                    return result.includeCurrentValue(credentialsId);
+                }
+            } else {
+                if (!item.hasPermission(Item.EXTENDED_READ) && !item.hasPermission(CredentialsProvider.USE_ITEM)) {
+                    return result.includeCurrentValue(credentialsId);
+                }
+            }
+            return result
+            .includeEmptyValue()
+            .includeMatchingAs(ACL.SYSTEM, item, StringCredentials.class, Collections.emptyList(), CredentialsMatchers.always())
+            .includeCurrentValue(credentialsId);
+        }
+
+        public FormValidation doCheckCredentialsId(@AncestorInPath Item item, @QueryParameter String value) {
+            if (item == null) {
+                if (!Jenkins.get().hasPermission(Jenkins.ADMINISTER)) {
+                    return FormValidation.ok();
+                }
+            } else {
+                if (!item.hasPermission(Item.EXTENDED_READ) && !item.hasPermission(CredentialsProvider.USE_ITEM)) {
+                    return FormValidation.ok();
+                }
+            }
+            if (StringUtils.isBlank(value)) {
+                return FormValidation.error("Cannot be empty");
+            }
+            if (CredentialsProvider.listCredentials(
+                StringCredentials.class,
+                item,
+                ACL.SYSTEM,
+                Collections.emptyList(),
+                CredentialsMatchers.withId(value)
+            ).isEmpty()) {
+                return FormValidation.error("Cannot find currently selected credentials");
+            }
+            return FormValidation.ok();
+        }
+
         public FormValidation doCheckAutifyUrl(@QueryParameter String value, @QueryParameter boolean wait) throws IOException, ServletException {
+            if (StringUtils.isBlank(value)) {
+                return FormValidation.error("Cannot be empty");
+            }
             return FormValidation.ok();
         }
 
--- src/test/java/io/jenkins/plugins/autify/AutifyWebBuilderTest.java
+++ src/test/java/io/jenkins/plugins/autify/AutifyWebBuilderTest.java
@@ -3,12 +3,20 @@ package io.jenkins.plugins.autify;
 import hudson.model.FreeStyleBuild;
 import hudson.model.FreeStyleProject;
 import hudson.model.Label;
+import hudson.util.Secret;
+
+import org.jenkinsci.plugins.plaincredentials.StringCredentials;
+import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
+
+import com.cloudbees.plugins.credentials.CredentialsScope;
+import com.cloudbees.plugins.credentials.SystemCredentialsProvider;
 
 public class AutifyWebBuilderTest {
 
@@ -16,24 +24,33 @@ public class AutifyWebBuilderTest {
     public JenkinsRule jenkins = new JenkinsRule();
 
     final String autifyUrl = "https://app.autify.com/projects/743/scenarios/91437";
+    final String credentialsId = "autifyWeb";
+    final String accessToken = "token";
+
+
+    @Before
+    public void setup() {
+        StringCredentials credentials = new StringCredentialsImpl(CredentialsScope.GLOBAL, credentialsId, "", Secret.fromString(accessToken));
+        SystemCredentialsProvider.getInstance().getCredentials().add(credentials);
+    }
 
     @Test
     public void testConfigRoundtrip() throws Exception {
         FreeStyleProject project = jenkins.createFreeStyleProject();
-        project.getBuildersList().add(new AutifyWebBuilder(autifyUrl));
+        project.getBuildersList().add(new AutifyWebBuilder(credentialsId, autifyUrl));
         project = jenkins.configRoundtrip(project);
-        jenkins.assertEqualDataBoundBeans(new AutifyWebBuilder(autifyUrl), project.getBuildersList().get(0));
+        jenkins.assertEqualDataBoundBeans(new AutifyWebBuilder(credentialsId, autifyUrl), project.getBuildersList().get(0));
     }
 
     @Test
     public void testConfigRoundtripWait() throws Exception {
         FreeStyleProject project = jenkins.createFreeStyleProject();
-        AutifyWebBuilder builder = new AutifyWebBuilder(autifyUrl);
+        AutifyWebBuilder builder = new AutifyWebBuilder(credentialsId, autifyUrl);
         builder.setWait(true);
         project.getBuildersList().add(builder);
         project = jenkins.configRoundtrip(project);
 
-        AutifyWebBuilder lhs = new AutifyWebBuilder(autifyUrl);
+        AutifyWebBuilder lhs = new AutifyWebBuilder(credentialsId, autifyUrl);
         lhs.setWait(true);
         jenkins.assertEqualDataBoundBeans(lhs, project.getBuildersList().get(0));
     }
@@ -41,7 +58,7 @@ public class AutifyWebBuilderTest {
     @Test
     public void testBuild() throws Exception {
         FreeStyleProject project = jenkins.createFreeStyleProject();
-        AutifyWebBuilder builder = new AutifyWebBuilder(autifyUrl);
+        AutifyWebBuilder builder = new AutifyWebBuilder(credentialsId, autifyUrl);
         project.getBuildersList().add(builder);
 
         FreeStyleBuild build = jenkins.buildAndAssertSuccess(project);
@@ -51,7 +68,7 @@ public class AutifyWebBuilderTest {
     @Test
     public void testBuildWait() throws Exception {
         FreeStyleProject project = jenkins.createFreeStyleProject();
-        AutifyWebBuilder builder = new AutifyWebBuilder(autifyUrl);
+        AutifyWebBuilder builder = new AutifyWebBuilder(credentialsId, autifyUrl);
         builder.setWait(true);
         project.getBuildersList().add(builder);
 
@@ -67,7 +84,7 @@ public class AutifyWebBuilderTest {
         WorkflowJob job = jenkins.createProject(WorkflowJob.class, "test-scripted-pipeline");
         String pipelineScript
                 = "node {\n"
-                + "  autifyWeb '" + autifyUrl + "'\n"
+                + "  autifyWeb credentialsId: '" + credentialsId + "', autifyUrl: '"+ autifyUrl +"'\n"
                 + "}";
         job.setDefinition(new CpsFlowDefinition(pipelineScript, true));
         WorkflowRun completedBuild = jenkins.assertBuildStatusSuccess(job.scheduleBuild2(0));
