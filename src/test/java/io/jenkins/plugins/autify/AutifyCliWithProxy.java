b4588ff92881cd8e1317b23d2e2dd26a0e0b4cd7
Ryosuke Iwanaga <ryosuke.iwanaga@autify.com>
2022-08-10 15:07:52 -0700

    Use frontend-maven-plugin instead of nvm

--- src/test/java/io/jenkins/plugins/autify/AutifyCliWithProxy.java
+++ src/test/java/io/jenkins/plugins/autify/AutifyCliWithProxy.java
@@ -9,23 +9,19 @@ import hudson.model.TaskListener;
 public class AutifyCliWithProxy extends AutifyCli {
 
     private final String originalAutifyPath;
+    private final String repositoryRoot;
 
     public AutifyCliWithProxy(FilePath workspace, Launcher launcher, TaskListener listener) {
         super(workspace, launcher, listener);
+        this.repositoryRoot = System.getProperty("user.dir");
         this.originalAutifyPath = autifyPath;
-        this.autifyPath = "./bin/autify-with-proxy";
-    }
-
-    public int install() {
-        int ret = runShellScript("AutifyCliWithProxy/install-proxy.sh");
-        if (ret != 0) return ret;
-        return super.install();
+        this.autifyPath = repositoryRoot + "/node_modules/.bin/autify-with-proxy";
     }
 
     protected Map<String, String> getEnvs() {
         Map<String, String> envs = super.getEnvs();
         envs.put("AUTIFY_CLI_PATH", originalAutifyPath);
-        envs.put("NVM_DIR", "./nvm");
+        envs.put("PATH+NODE", repositoryRoot + "/node");
         return envs;
     }
 
