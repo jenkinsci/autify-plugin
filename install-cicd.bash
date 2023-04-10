7ce44d9ad67319173377de271eb891b763cfdf64
Fumiaki MATSUSHIMA <mtsmfm@gmail.com>
2023-04-10 17:37:24 +0900

    wip

--- install-cicd.bash
+++ install-cicd.bash
@@ -50,7 +50,7 @@ if [ "$OS" == "windows" ]; then
   echo "Installing CLI from $URL"
   EXE_FILE="$AUTIFY_DIR/installer.exe"
   curl "$URL" > "$EXE_FILE"
-  cd $AUTIFY_DIR
+  cd "$AUTIFY_DIR"
   cmd.exe /C installer.exe /S /D=.
   cd -
 else
